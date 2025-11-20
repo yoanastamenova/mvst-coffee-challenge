401f57cd645466dfa590eb1ebc2b020461ce2e1b
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-11-20 17:13:50 +0100

    feat(database): initialize TypeORM with PostgreSQL configuration with Coffee entity model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'postgres'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: ['dist/database/models/**/*.entity.js'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
 })
 export class AppModule {}
---
+++ backend/src/database/db.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [`${__dirname}/models/**/*{.ts,.js}`],
+  migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+  synchronize: false,
+  logging: false,
+});
