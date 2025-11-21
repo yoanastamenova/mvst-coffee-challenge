9f9ad36edae46f44e2a748c132498675e48c05f0
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-11-21 11:48:27 +0100

    feat: add coffees controller and test list show and new coffee creation

--- backend/src/coffees/coffees.controller.ts
+++ backend/src/coffees/coffees.controller.ts
@@ -1,34 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
-import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
-  @Post()
-  create(@Body() createCoffeeDto: CreateCoffeeDto) {
-    return this.coffeesService.create(createCoffeeDto);
-  }
-
   @Get()
-  findAll() {
+  async findAll(): Promise<Coffee[]> {
     return this.coffeesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
-    return this.coffeesService.update(+id, updateCoffeeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.coffeesService.remove(+id);
+  @Post('new')
+  async create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    const coffee = await this.coffeesService.create(createCoffeeDto);
+    return {
+      message: `Success! New coffee saved: ${coffee.name}`,
+      coffee,
+    };
   }
 }
