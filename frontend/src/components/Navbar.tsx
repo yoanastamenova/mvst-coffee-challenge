6ecf1d50968ed52e374efaf7a163fa914571d48b
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-11-22 17:27:17 +0100

    feat: add Navbar with logo to header

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const Navbar = () => {
+    return(
+        <nav className="sticky top-0 z-50 bg-gray-900 shadow-md">
+          <Link href="/" className="hover:opacity-80 px-6 py-4 block">
+            <Image
+              src="/MVST_header.svg"
+              width={167}
+              height={25}
+              alt="Header Logo"
+            />
+          </Link>
+        </nav>
+    )
+}
