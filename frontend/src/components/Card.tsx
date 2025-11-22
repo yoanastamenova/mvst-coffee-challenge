8c6a827834667ce625024f4ec3810db3f2a9e98e
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-11-22 17:31:50 +0100

    feat: add background to all page

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const Card = ({ name, description, price, imageUrl }: Props) => {
   return (
-    <div className='border border-black'>
+    <div className='border border-white bg-amber-900'>
       <h1 className='text-5xl text-primary'>{name}</h1>
       <p className='mt-2 text-sm text-grey'>{description}</p>
       <p className='text-2xl font-bold'>{price}</p>
